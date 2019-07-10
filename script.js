3024134522a7e887c4a1cec723cad73de901907f
Apple <apple@Apples-MacBook-Pro-15.local>
2019-07-10 17:40:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+p {
+  border-top-style: dotted;
+  border-right-style: solid;
+  border-bottom-style: dotted;
+  border-left-style: solid;
+  border: 5px solid red;
+}
+
+h1 {
+  background-color: lightblue;
+    text-decoration: underline;
+}
